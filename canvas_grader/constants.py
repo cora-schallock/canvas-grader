08424292defb9ad93b1c6f34e1e06cda94626d45
Cora Schallock <coraschallock@gmail.com>
2022-11-02 15:24:51 -0700

    updating with seating assignment

--- canvas_grader/constants.py
+++ canvas_grader/constants.py
@@ -1,8 +1,8 @@
 #Variables:
 #Update these based on your specific setup/shool/course
 CANVAS_URL = "https://canvas.eee.uci.edu" #your school's canvas URL
-COURSE_CODE = 44461 #your course code
-PATH_TO_CANVAS_KEY_FILE = "keys//canvas-api-key.txt" #the file path of your canvas api key (where you saved it from the setup)
+COURSE_CODE = 48117 #your course code
+PATH_TO_CANVAS_KEY_FILE = "keys//ics_6d_fall_2022_key.txt" #the file path of your canvas api key (where you saved it from the setup)
 
 #Course Roster CSV KEYS:
 #Important: do NOT edit this section unless you know what you're doing
--- canvas_grader/create_comment_files.py
+++ canvas_grader/create_comment_files.py
@@ -2,6 +2,7 @@ from canvas_grader.constants import STUDENT_KEY, ID_KEY, SIS_USER_ID_KEY, SIS_LO
 from canvas_grader.file_helpers import write_to_file, check_if_folder_exists_and_create, comment_file_path
 from canvas_grader.process_gradebook import standardize
 
+#comment:
 def constuct_comment(student,sis_login_id,section,assignment_name,row,header_dict,score_key="total"):
     the_comment = []
     
@@ -29,6 +30,41 @@ def create_comment_files(data_frame,output_comment_folder,header_dict={},score_k
             section = row[SECTION_KEY]
         
             the_comment = constuct_comment(student,sis_login_id,section,assignment_name,row,header_dict,score_key="total")
+        
+            comment_path = comment_file_path(output_comment_folder,sis_login_id,assignment_name)
+            write_to_file(comment_path,the_comment)
+        except Exception as e:
+            print("Error while creating comment for student={}, {}".format(student,e))
+            
+            
+#seating:
+def constuct_seat_comment(student,sis_login_id,section,assignment_name,row,header_dict,room_string=""):
+    the_comment = []
+    
+    the_comment = [assignment_name]
+    the_comment.append("\tSection:{}".format(section))
+    the_comment.append("\t\t-Student: {} ({})".format(student,sis_login_id))
+    the_comment.append("")
+    
+    for col_name in header_dict:
+        the_comment.append('{}: {}'.format(header_dict[col_name],standardize(row[col_name])))
+        
+    the_comment.append("Information: {}".format(room_string))
+            
+    return the_comment
+
+def create_seat_comment_files(data_frame,output_comment_folder,header_dict={},room_string="",assignment_name="assignment"):
+    check_if_folder_exists_and_create(output_comment_folder)
+    
+    for index, row in data_frame.iterrows():
+        student="*missing name*" #incase error and missing name
+        
+        try:
+            student = row[STUDENT_KEY]
+            sis_login_id = row[SIS_LOGIN_ID_KEY]
+            section = row[SECTION_KEY]
+        
+            the_comment = constuct_seat_comment(student,sis_login_id,section,assignment_name,row,header_dict,room_string)
         
             comment_path = comment_file_path(output_comment_folder,sis_login_id,assignment_name)
             write_to_file(comment_path,the_comment)
